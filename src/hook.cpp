e184fb37753748332b35fd034c1aab796c447650
LeoneKingzz <167722980+LeoneKingzz@users.noreply.github.com>
2024-10-21 17:30:26 +0100

    Update hook.cpp

--- src/hook.cpp
+++ src/hook.cpp
@@ -84,7 +84,7 @@ namespace hooks
 				effect->effectItem.area = 0;
 				effect->effectItem.duration = 0;
 				effect->effectItem.magnitude = 0;
-				effect->baseEffect->data.archetype = RE::EffectSetting::Archetype::kScript;
+				//effect->baseEffect->data.archetype = RE::EffectSetting::Archetype::kScript;
 				effect->baseEffect->conditions.head = new RE::TESConditionItem;
 				effect->baseEffect->conditions.head->data.comparisonValue.f = 6.0f;
 				effect->baseEffect->conditions.head->data.functionData.function = RE::FUNCTION_DATA::FunctionID::kGetRandomPercent;
